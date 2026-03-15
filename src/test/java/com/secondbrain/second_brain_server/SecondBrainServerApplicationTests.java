a1dd620d2bb75c9bc31153314a8a12f99831a481
prajwalp11 <prajwalp1106@gmail.com>
2026-03-15 07:08:07 +0000

    Initial commit : Setup spring boot project - second brain server

---
+++ src/test/java/com/secondbrain/second_brain_server/SecondBrainServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secondbrain.second_brain_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondBrainServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
