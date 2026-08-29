d6ce6a7db7c9acf073bf2f200d99ad32dfe801dd
Prajwal P <prajwalp1106@gmail.com>
2026-08-29 21:12:49 +0530

    Fix: Scheduling the cron to run

--- src/main/java/com/secondbrain/second_brain_server/SecondBrainServerApplication.java
+++ src/main/java/com/secondbrain/second_brain_server/SecondBrainServerApplication.java
@@ -2,8 +2,12 @@ package com.secondbrain.second_brain_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableAsync
 public class SecondBrainServerApplication {
 
 	public static void main(String[] args) {
